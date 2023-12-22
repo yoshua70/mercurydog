ec563a990527be651cf1b582a7daf94f0a8a5a81
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-12-22 16:31:37 +0100

    Add test for when the db file already exists

--- cmd/init_test.go
+++ cmd/init_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestCreateDb_WhenFileDoesExist_ShouldSucceed(t *testing.T) {
+func TestCreateDb_WhenFileDoesNotExist_ShouldSucceed(t *testing.T) {
 	dbName := "tau.db"
 	// Remove the file to be sure the test is executed in a clean state.
 	os.Remove(fmt.Sprintf("./%v", dbName))
@@ -19,4 +19,17 @@ func TestCreateDb_WhenFileDoesExist_ShouldSucceed(t *testing.T) {
 	// Clean up after the test, leave the filesystem as it was before the test
 	// was executed.
 	os.Remove(fmt.Sprintf("./%v", dbName))
+}
+
+func TestCreateDb_WhenFileDoesExist_ShouldNotSucceed(t *testing.T) {
+	dbName := "tau.db"
+	os.Create(fmt.Sprintf("./%s", dbName))
+
+	err := CreateDb(dbName)
+
+	if err == nil {
+		t.Errorf("write operation to create database when the file already exists should not have succeeded")
+	}
+
+	os.Remove(fmt.Sprintf("./%s", dbName))
 }
